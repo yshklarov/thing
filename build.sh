9b5049656363c331af06605cd7d710063f002a8e
Yakov Shklarov <yakov@shklarov.com>
2024-04-29 20:31:39 -0700

    Update build flags

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-WARNING_FLAGS="-pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-declarations -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wno-unused"
+WARNING_FLAGS="-pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wmissing-declarations -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused"
+
+LINKER_FLAGS="-lraylib"
+
+COMPILER_FLAGS="-O3"
 
 CPPSTD="c++17"
 
-clang++ -std=$CPPSTD $WARNING_FLAGS -O3 thing.cpp -o build/thing -lraylib && ./build/thing
+clang++ -std=$CPPSTD $WARNING_FLAGS $COMPILER_FLAGS $LINKER_FLAGS thing.cpp -o build/thing && ./build/thing
